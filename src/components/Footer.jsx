cf2ee98e9ba30d09dc595395a35a63a9b6645c66
wsnrfidev <bisabismillah04@gmail.com>
2024-12-09 20:51:04 +0700

    update desc

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,8 +24,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+62-123-456-789</li>
+            <li>contactus@example.com</li>
           </ul>
         </div>
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,8 +13,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
+          <p className=' text-gray-500'>Ruko Blok C No. 789 <br /> Bsd City, Tangerang Kota, Banten</p>
+          <p className=' text-gray-500'>Tel: (62) 123-456-789 <br /> Email: contactus@example.com</p>
           <p className=' font-semibold text-lg text-gray-600'>CAREERS AT YESDOC</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
